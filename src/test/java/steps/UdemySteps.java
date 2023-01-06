687328709b419176eb3f83ca9fe5f97620b50dcf
valendiaz7 <valentin.diaz.33533@gmail.com>
2023-01-06 20:08:51 -0300

    agrego steps definitions utilizando page objtec model

--- src/test/java/steps/UdemySteps.java
+++ src/test/java/steps/UdemySteps.java
@@ -1,4 +1,39 @@
 package steps;
 
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import pages.UdemyPage;
+
 public class UdemySteps {
+
+    UdemyPage Udemy = new UdemyPage();
+
+    @Given("^Ingresamos a la home de Udemy$")
+    public void navigateToUdemy() {
+        Udemy.navigateToUdemy();
+    }
+
+    @When("^Ingreso un criterio de busqueda$")
+    public void enterSearchCriteria() {
+        Udemy.enterSearchCriteria();
+    }
+
+    @And("^Clickeo en el boton search$")
+    public void clickButtonSearch() {
+        Udemy.clickButton();
+    }
+
+    @And("^Aplico filtros ordenar por e idioma$")
+    public void aplyFilters() {
+        Udemy.aplyFilters();
+    }
+
+    @Then("^Valido la cantidad de cursos disponibles$")
+    public void validateResults() {
+        Udemy.validateResults();
+    }
+
+
 }
